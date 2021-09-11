958a4ca8ed695ea987854310ad163960f470fd27
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-09-11 18:50:41 -0500

    Fixed configureStore required params

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,8 +4,8 @@ import reducer from "./bugs";
 import { configureStore } from "@reduxjs/toolkit";
 
 export default function () {
-  const store = configureStore(
+  const store = configureStore({
     reducer
-  );
+  });
   return store;  
 };
