9ed5775c9843fce6a347a9cb61759e9ef1fb5df1
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-09-13 22:21:37 -0500

    added/dispached new project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import configureStore from "./store/configureStore";
 import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
@@ -9,8 +10,12 @@ const unsubscribe = store.subscribe(() => {
 
 console.log(store.getState());
 
+store.dispatch(projectAdded({name: 'My First Project'})); 
+
 store.dispatch(bugAdded({description: 'My first bug'}));
 
 store.dispatch(bugResolved({id: 1}));
 
 store.dispatch(bugRemoved({id: 1}));
+
+
