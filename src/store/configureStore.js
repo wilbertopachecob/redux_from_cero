a5314b0c499ed53c6ef559a2befa6d6c1d5d37aa
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-09-13 22:21:15 -0500

    imported new reducer file

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,6 @@
 // import { createStore } from "redux";
 // import createStore from "./customStore";
-import reducer from "./bugs";
+import reducer from "./reducer.js";
 import { configureStore } from "@reduxjs/toolkit";
 
 export default function () {
