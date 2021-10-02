ab57939edcfc6e4c0fc826efa689851c27f57de2
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-02 12:41:56 -0500

    Refactored logger middleware

--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -1,8 +1,8 @@
-const logger = type => store => next => action => {
-    console.log({
-        type
-    });
-    next(action);
-}
+const logger = (type) => (store) => (next) => (action) => {
+  const specialTypes = ['error', 'info', 'warn'];
+  const method = specialTypes.find(st => st === type) || 'log';
+  console[method](action.type);
+  next(action);
+};
 
 export default logger;
