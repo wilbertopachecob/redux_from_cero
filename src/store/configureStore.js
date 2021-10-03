140af043ead043bf70f60b1c09291a715aa07033
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-03 14:26:08 -0500

    Added api middleware to store config

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,6 +4,7 @@ import reducer from "./reducer.js";
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "./middleware/logger";
 import toast from "./middleware/toast";
+import api from "./middleware/api";
 // import func from "./middleware/func";
 //so redux/toolkit uses a middleware called thunk that does what our func middleware does, we just to pass it to the store config as a function,
 //its included in the getDefaultMiddleware array
@@ -13,7 +14,7 @@ export default function () {
     reducer,
     // middleware: [logger("info"), func],
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(logger("info"), toast),
+      getDefaultMiddleware().concat(logger("info"), toast, api),
   });
   return store;
 }
