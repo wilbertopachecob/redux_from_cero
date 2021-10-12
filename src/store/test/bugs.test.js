29c203d4e7ed13348e732c76fb885faa45b682cd
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-11 23:24:49 -0500

    Added mockFetch function. Added test for resolveBug action

--- src/store/test/bugs.test.js
+++ src/store/test/bugs.test.js
@@ -1,5 +1,5 @@
 import configureStore from "../configureStore";
-import { addBug } from "../bugs";
+import { addBug, resolveBug } from "../bugs";
 
 describe("bugsSlice - ", () => {
   let store;
@@ -8,25 +8,48 @@ describe("bugsSlice - ", () => {
   });
 
   const bugsSlice = () => store.getState().entities.bugs;
+  const mockFetch = (result, status = 200, error = null) =>
+    jest.fn(() => {
+      if (status === 200) {
+        return Promise.resolve({ json: () => result });
+      }
+      return Promise.reject(error);
+    });
 
   it("should add the bug to the store if its saved to the server", async () => {
     //appliying Arrange/Act/Assert or AAA
     const bug = { description: "a" };
-    const savedbug = { ...bug, id: 1 };
-    global.fetch = jest.fn(() => Promise.resolve({ json: () => savedbug }));
+    const savedBug = { ...bug, id: 1 };
+    global.fetch = mockFetch(savedBug);
 
     await store.dispatch(addBug(bug));
 
     expect(bugsSlice().list).toHaveLength(1);
-    expect(bugsSlice().list).toContainEqual(savedbug);
+    expect(bugsSlice().list).toContainEqual(savedBug);
   });
 
   it("should not add the bug to the store if its saved to the server", async () => {
     const bug = { description: "a" };
-    global.fetch = jest.fn(() => Promise.reject(new Error("Network error")));
+    global.fetch = mockFetch(null, 404, new Error("Network error"));
 
     await store.dispatch(addBug(bug));
 
     expect(bugsSlice().list).toHaveLength(0);
+  });
+
+  it("should resolve a bug", async () => {
+    const bug = { description: "a" };
+    const savedBug = { ...bug, id: 1, resolved: false };
+    global.fetch = mockFetch(savedBug);
+
+    await store.dispatch(addBug(bug));
+
+    expect(bugsSlice().list).toHaveLength(1);
+
+    const resolvedBug = { ...savedBug, resolved: true };
+    global.fetch = mockFetch(resolvedBug);
+
+    await store.dispatch(resolveBug(savedBug.id));
+    expect(bugsSlice().list[0]).toEqual(resolvedBug);
   });
 });
