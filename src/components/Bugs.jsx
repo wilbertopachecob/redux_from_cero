77417ef2011e5803582f85da79d891f3c816fdec
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-16 20:40:19 -0500

    Manually adding store to the bugs component

--- src/components/Bugs.jsx
+++ src/components/Bugs.jsx
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import StoreContext from "../contexts/storeContext";
+import { loadBugs } from "../store/bugs";
+import { isFunction as _isFunction } from "lodash";
 
 class Bugs extends Component {
-    render() {
-        return (
-            <div>
-                Bugs here!
-            </div>
-        );
+  static contextType = StoreContext;
+  state = { bugs: [] };
+
+  componentDidMount() {
+    const store = this.context;
+    this.unsubscribe = store.subscribe(() => {
+      const bugsInStore = store.getState().entities.bugs.list;
+      if (this.state.bugs !== bugsInStore) {
+        this.setState({ bugs: bugsInStore });
+      }
+    });
+    store.dispatch(loadBugs());
+  }
+
+  componentWillUnmount() {
+    if (_isFunction(this.unsubscribe)) {
+      this.unsubscribe();
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.bugs.map((bug) => (
+            <li key={bug.id}>{bug.description}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Bugs;
